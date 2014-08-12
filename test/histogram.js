73ce8976ab24a7839341e729a2621d435d809fb0
Peter Müller <munter@fumle.dk>
2014-08-12 23:32:28 +0200

    Rewrite test suite to mocha

---
+++ test/histogram.js
@@ -0,0 +1,63 @@
+var expect = require('unexpected'),
+    histogram = require('../lib/index.js');
+
+describe('api', function () {
+    it('should throw on invalid image buffer argument', function () {
+        expect(histogram, 'to throw', function (err) {
+            expect(err, 'to be a', TypeError);
+            expect(err.message, 'to be', 'Expected imageBuffer to be a string or a buffer');
+        });
+    });
+
+    it('should throw on missing callback argument', function () {
+        expect(histogram.bind(this, 'foo'), 'to throw', function (err) {
+            expect(err, 'to be a', TypeError);
+            expect(err.message, 'to be', 'Expected callback to be a function');
+        });
+    });
+
+    it('should return an error when given an invalid image path', function (done) {
+        histogram('invalidFilePath', function (err, result) {
+            expect(result, 'to be undefined');
+            expect(err, 'to be a', Error);
+
+            done();
+        });
+    });
+});
+
+describe('Histogram of gradient.png', function () {
+    var path = __dirname + '/images/gradient.png';
+
+    it('should be greyscale', function (done) {
+        histogram(path, function (error, result) {
+            expect(result.greyscale, 'to be true');
+
+            done();
+        });
+    });
+
+    it('should have an alpha channel', function (done) {
+        histogram(path, function (error, result) {
+            expect(result.alphachannel, 'to be true');
+
+            done();
+        });
+    });
+
+    it('should have 256 alpha channel colors', function (done) {
+        histogram(path, function (error, result) {
+            expect(result.colors.rgba, 'to be', 256);
+
+            done();
+        });
+    });
+
+    it('1hould have 1 rgba color', function (done) {
+        histogram(path, function (error, result) {
+            expect(result.colors.rgb, 'to be', 1);
+
+            done();
+        });
+    });
+});
